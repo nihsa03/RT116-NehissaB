b286d10fc478a6d51bd69d5b88a4ff79f8268097
nihsa03 <nehissab@icloud.com>
2024-10-10 12:45:45 -0400

    M303_9 Homework updates

--- rtt Modules/src/Homework/M303_9/GLAB_303_9.java
+++ rtt Modules/src/Homework/M303_9/GLAB_303_9.java
@@ -1,81 +1,56 @@
 package Homework.M303_9;
 
-public class GLAB_303_9 { // Main class
+public class GLAB_303_9 { // Combined class for encapsulation example
 
     // HumanBeing class demonstrating encapsulation
-    static class HumanBeing {
-        private float weight;
-        private float height;
-        private float bmi;
+    private float weight;
+    private float height;
+    private float bmi;
 
-        public HumanBeing(float weight, float height) {
-            this.weight = weight;
-            this.height = height;
-            this.bmi = calculateBmi();
-        }
-
-        public HumanBeing() {
-            // Default constructor
-        }
-
-        public float getWeight() {
-            return weight;
-        }
-
-        public void setWeight(float weight) {
-            this.weight = weight;
-            this.bmi = calculateBmi(); // Update BMI when weight is set
-        }
-
-        public float getHeight() {
-            return height;
-        }
-
-        public void setHeight(float height) {
-            this.height = height;
-            this.bmi = calculateBmi(); // Update BMI when height is set
-        }
-
-        public float getBmi() {
-            return bmi;
-        }
-
-        private float calculateBmi() {
-            return weight / (height * height);
-        }
+    public GLAB_303_9(float weight, float height) {
+        this.weight = weight;
+        this.height = height;
+        this.bmi = calculateBmi();
     }
 
-    // Doctor class demonstrating inheritance
-    static class Doctor {
-        String doctorName;
-        String department;
-
-        public void doctorDetails() {
-            System.out.println("Doctor Details...");
-        }
+    public GLAB_303_9() {
+        // Default constructor
     }
 
-    // Surgeon class that inherits from Doctor
-    static class Surgeon extends Doctor {
-        public void surgeonDetails() {
-            System.out.println("Surgeon Detail...");
-            department = "Cardio"; // Set department
-            System.out.println("Department: " + department);
-        }
+    public float getWeight() {
+        return weight;
     }
 
-    // Main class to run the program
+    public void setWeight(float weight) {
+        this.weight = weight;
+        this.bmi = calculateBmi(); // Update BMI when weight is set
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+        this.bmi = calculateBmi(); // Update BMI when height is set
+    }
+
+    public float getBmi() {
+        return bmi;
+    }
+
+    private float calculateBmi() {
+        return weight / (height * height);
+    }
+
+    // Main method to demonstrate encapsulation
     public static void main(String[] args) {
-        // Demonstrating HumanBeing encapsulation
-        HumanBeing h1 = new HumanBeing();
+        GLAB_303_9 h1 = new GLAB_303_9();
+        // Using setters of GLAB_303_9
         h1.setHeight(1.65f);
         h1.setWeight(68);
 
+        // Using getters of GLAB_303_9
         System.out.println("Person has " + h1.getWeight() + " kgs and is " + h1.getHeight() + " meters in height, which results in BMI of " + h1.getBmi());
-
-        // Demonstrating inheritance
-        Surgeon s = new Surgeon();
-        s.doctorDetails();
-        s.surgeonDetails();
     }
 }
